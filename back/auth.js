8d67af1836fd84aa58669dc458004dc1219768ef
Gael Castro <gaelcastrolara2006@gmail.com>
2026-02-08 13:22:03 -0600

    a

--- back/auth.js
+++ back/auth.js
@@ -5,30 +5,18 @@ const router = express.Router();
 router.post('/', (req, res) => {
     const { username, password } = req.body;
 
-    //simulacion de validacion en caso de que se maneje una base de datos
-    if ( username === 'admin' && password === '1234') {
-
-        //crear la sesion
-        req.session.user = { id: 1, name: 'admin' };
-        return res.status(200).json({ message: 'Inicio de sesión exitoso' });
+    // Simulación de base de datos con roles
+    if (username === 'admin' && password === '1234') {
+        req.session.user = { id: 1, name: 'admin', role: 'admin' }; // Guardamos el rol admin
+        return res.status(200).json({ message: 'Bienvenido, admin' });
+    } 
+    else if (username === 'user' && password === '5678') {
+        req.session.user = { id: 2, name: 'user', role: 'viewer' }; // Usuario normal
+        return res.status(200).json({ message: 'Bienvenido, Usuario' });
     }
 
-    return res.status(401).json({
-        error: 'Credenciales inválidas'
-    });
+    return res.status(401).json({ error: 'Credenciales inválidas' });
 });
 
-router.post('/register', (req, res) => {
-    const { username, password } = req.body;
-
-    //simulacion de validacion en caso de que se maneje una base de datos
-    if ( username === 'admin' && password === '1234') {
-        return res.status(200).json({ message: 'Registro exitoso' });
-    }
-
-    return res.status(401).json({
-        error: 'Error en el registro'
-    });
-});
 
 module.exports = router;
--- front/script.js
+++ front/script.js
@@ -195,7 +195,7 @@ function renderTasks() {
                     <div class="task-details">
                         <div class="task-detail">
                             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                                <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                                <rect width="18" height="18" rx="2" ry="2"></rect>
                                 <line x1="16" y1="2" x2="16" y2="6"></line>
                                 <line x1="8" y1="2" x2="8" y2="6"></line>
                                 <line x1="3" y1="10" x2="21" y2="10"></line>
@@ -220,7 +220,7 @@ function renderTasks() {
                         </div>
                         <div class="task-detail">
                             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                                <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                                <rect width="18" height="18" rx="2" ry="2"></rect>
                                 <line x1="16" y1="2" x2="16" y2="6"></line>
                                 <line x1="8" y1="2" x2="8" y2="6"></line>
                                 <line x1="3" y1="10" x2="21" y2="10"></line>
